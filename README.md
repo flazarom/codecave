92dd62ab242debc5c54f114994e396d5e4d1e1cb
salinas38m <salinas38m@gmail.com>
2020-02-16 02:14:45 -0300

    navbar responsive

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Cavecode
+# codecave
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cavecode'`, () => {
+  it(`should have as title 'codecave'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cavecode');
+    expect(app.title).toEqual('codecave');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cavecode app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('codecave app is running!');
   });
 });
